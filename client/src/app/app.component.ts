bd333df1e288026276981fe7cf2bc838c9b1191b
Dmytro <lytvyniuk.dima@gmail.com>
2021-05-29 16:15:05 +0300

    added prod build

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,8 +5,4 @@ import { Component } from '@angular/core';
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-	constructor() {
-		console.log("Hello, tic-tac-toe");
-	}
-}
+export class AppComponent {}
